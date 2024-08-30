a1fd54bf46dd3a4f921d5cf67cc493c60ff1be8e
Basem-Mouner <bmr.audi@gmail.com>
2024-08-30 15:06:15 +0300

    forth commit

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -37,7 +37,7 @@ export default function Checkout() {
     },
 
     onSubmit: () => {
-      handlecheckout(cart.data._id, "https://fresh-cart-puce.vercel.app/#");
+      handlecheckout(cart.data._id, "https://fresh-cart-puce.vercel.app#/");
     },
   });
 
